ea042cf44c52d7a45af49ee06706f08f9f182624
Philip James <phildini@phildini.net>
2026-06-12 05:40:33 +0100

    Redirect root to /admin/

--- comitatus/urls.py
+++ comitatus/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import path
 
 urlpatterns = [
+    path("", lambda request: HttpResponseRedirect("/admin/")),
     path("admin/", admin.site.urls),
 ]
