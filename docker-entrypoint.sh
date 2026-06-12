9cf119ab332aa4381c05bf55ede8e19ed8a34104
Philip James <phildini@phildini.net>
2026-06-12 05:43:56 +0100

    Run collectstatic in entrypoint so Whitenoise serves admin assets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,5 @@ db_dir=$(dirname "$db_path")
 mkdir -p "$db_dir"
 
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
 exec gunicorn comitatus.wsgi --bind 0.0.0.0:8080 --workers 2 --timeout 120
